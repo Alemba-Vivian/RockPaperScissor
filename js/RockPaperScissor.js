2be279ae5e631dfb6b00a3e086b12068f8361c38
Alemba-Vivian <alembavivian@gmail.com>
2023-05-02 17:28:49 +0300

    Added another function that stores the scores and declare the winner

--- js/RockPaperScissor.js
+++ js/RockPaperScissor.js
@@ -1,3 +1,4 @@
+//randomly get the computer input
 function getComputerChoice(){
     const computerChoice =['rock','paper','scissors']
     const randomIndexValue =Math.floor(Math.random() *computerChoice.length)
@@ -6,21 +7,35 @@ function getComputerChoice(){
     return randomComputerChoice;
 }
 
+//ask the user to put either rock, paper or scissors
+function getPlayerSelection(){
+    let playerSelection =prompt("Let's play RockPaperScissor game, Enter Rock, Paper or Scissors", "")
+    return playerSelection.toLowerCase()
 
-function playRound(playerSelection, computerSelection){
-   if(playerSelection.toLowerCase() === 'paper' && computerSelection==='rock' 
-    || playerSelection.toLowerCase() === 'rock' && computerSelection==='scissors'
-    || playerSelection.toLowerCase() === 'scissors' && computerSelection==='paper')
+}
+
+
+function playRound(){
+    let playerSelection=getPlayerSelection()
+    console.log(`PlayerSelection is ${playerSelection}`)
+    let computerSelection = getComputerChoice()
+    console.log(`ComputerSelection is ${computerSelection}`)
+  
+   if(playerSelection=== 'paper' && computerSelection==='rock' 
+    || playerSelection=== 'rock' && computerSelection==='scissors'
+    || playerSelection=== 'scissors' && computerSelection==='paper')
     {
         playerScore++
         return `PlayerSelection wins and has ${playerScore} points`
-    }else if(playerSelection.toLowerCase() ==='rock' && computerSelection==='paper'
-    || playerSelection.toLowerCase() === 'scissors'&& computerSelection==='rock'
-    || playerSelection.toLowerCase() === 'paper' && computerSelection==='scissors')
+
+    }else if(playerSelection==='rock' && computerSelection==='paper'
+    || playerSelection=== 'scissors'&& computerSelection==='rock'
+    || playerSelection=== 'paper' && computerSelection==='scissors')
     {
         computerScore++
         return `ComputerSelection wins and has ${computerScore} points` 
-    }else if(playerSelection.toLowerCase() ===computerSelection)
+        
+    }else if(playerSelection===computerSelection)
     {
         tieScore
         return `Draw, Try Again, ${tieScore} points` 
@@ -30,40 +45,40 @@ function playRound(playerSelection, computerSelection){
     }
 
 }
-let playerSelection =prompt("Let's play RockPaperScissor game, Enter Rock, Paper or Scissors", "")
-console.log(`PlayerSelection is ${playerSelection}`)
-let computerSelection = getComputerChoice()
-console.log(`ComputerSelection is ${computerSelection}`)
+
+
 let playerScore = 0
 let computerScore= 0
 let tieScore = 0
 
 
-function game(){
-    console.log( playRound(playerSelection, computerSelection))
-    console.log( playRound(playerSelection, computerSelection))
-    console.log( playRound(playerSelection, computerSelection))
-    console.log( playRound(playerSelection, computerSelection))
-    console.log( playRound(playerSelection, computerSelection))
-    console.log(scores())
+function game(playerSelection,computerSelection){
+     console.log(playRound(playerSelection, computerSelection))
+     console.log(playRound(playerSelection, computerSelection))
+     console.log(playRound(playerSelection, computerSelection))
+     console.log(playRound(playerSelection, computerSelection))
+     console.log(playRound(playerSelection, computerSelection))
 }
 game()
+     let playerSelection =getPlayerSelection()
+     let computerSelection =getComputerChoice()
 
 function scores(){
 if(playerScore === computerScore)
 {
-    console.log(`The game is draw`)
+    console.log(`THE GAME IS DRAW WITH EACH ${tieScore} POINTS!!!!!!`)
     return tieScore
     
 }else if(playerScore > computerScore)
 {
-    console.log(`player wins the game`)
+    console.log(`PLAYER WINS WITH THE GAME WITH ${playerScore} POINTS!!!!!`)
     return playerScore
   
 }else{
-    console.log(`computer wins the game`)
+    console.log(`COMPUTER WINS THE GAME WITH ${computerScore} POINTS!!!!!`)
     return computerScore
 }
 
 }
+scores()
 
